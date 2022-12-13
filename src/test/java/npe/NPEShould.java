faa7887c97ed2daf7c42f5eaba19d28135178bdd
Farnam <farnam.hs@yahoo.com>
2022-12-13 17:33:40 +0330

    NPE

---
+++ src/test/java/npe/NPEShould.java
@@ -0,0 +1,12 @@
+package npe;
+
+import org.junit.jupiter.api.Test;
+
+public class NPEShould {
+
+    @Test
+    void be_helpful() {
+        String s = null;
+        s.toLowerCase().replace("a", "b").isBlank();    // throws NPE
+    }
+}
